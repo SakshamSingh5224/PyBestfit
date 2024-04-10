1bde60ef2364617483e84d939ff564674f347ad5
SakshamSingh5224 <121106258+SakshamSingh5224@users.noreply.github.com>
2024-04-11 01:13:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow  # Import specific classes
 # Import other necessary classes from ui_manager (if any)
-from ui_manager import *
 if __name__ == "__main__":
     app = QApplication(['BestFit Algorithm'])  # Initialize the application
     window = BestfitApp()  # Assuming BestfitApp is defined elsewhere
